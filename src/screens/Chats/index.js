170f5e04f4c3d07904c025195a54078b14a967d1
Vitor Leonel <falecom@vitorleonel.com>
2020-04-01 19:48:14 -0300

    implement text and button for empty chats

--- src/screens/Chats/index.js
+++ src/screens/Chats/index.js
@@ -1,14 +1,49 @@
 import React from "react";
-import { Text } from "react-native";
 
-import { Container } from "./styles";
+import { Container, Search, SearchInput, SearchAddChat } from "./styles";
+
+import Row from "../../components/Row";
+import Text from "../../components/Text";
+import Button from "../../components/Button";
+import Icon from "../../components/Icon";
 
 export default function Chats({ navigation }) {
+  function EmptyChats() {
+    return (
+      <Row flex={1} alignItems="center" justifyContent="center">
+        <Text color="#BABABA" align="center" lineHeight={20}>
+          Você ainda não faz parte de nenhum bate-papo. Crie um agora mesmo e
+          convide seus amigos!
+        </Text>
+
+        <Button
+          width={60}
+          text="Novo bate-papo"
+          leftIcon="plus-circle"
+          marginTop={32}
+        />
+      </Row>
+    );
+  }
+
   return (
     <Container>
-      <Text onPress={() => navigation.navigate("Conversation")}>
-        Ir para a conversa
-      </Text>
+      <Search>
+        <Icon name="search" size={22} color="#BABABA" marginLeft={12} />
+        <SearchInput placeholder="Encontre bate-papos" />
+
+        <SearchAddChat>
+          <Icon
+            name="plus-circle"
+            size={22}
+            color="#BABABA"
+            marginLeft={16}
+            marginRight={16}
+          />
+        </SearchAddChat>
+      </Search>
+
+      <EmptyChats />
     </Container>
   );
 }
--- src/screens/Chats/styles.js
+++ src/screens/Chats/styles.js
@@ -1,8 +1,49 @@
 import styled from "styled-components/native";
+import { Platform } from "react-native";
+import { getStatusBarHeight, isIphoneX } from "react-native-iphone-x-helper";
 
 export const Container = styled.View`
   flex: 1;
   background-color: #ffffff;
-  padding-top: 64px;
+  padding-top: ${isIphoneX() ? getStatusBarHeight() + 24 : 24}px;
   padding-horizontal: 24px;
+`;
+
+export const Search = styled.View`
+  background-color: #ffffff;
+  border-radius: 6px;
+  flex-direction: row;
+  align-items: center;
+
+  ${Platform.OS === "ios" &&
+    `
+    border-width: 1px;
+    border-color: rgba(0, 0, 0, 0.1);
+    box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.1);
+  `}
+
+  ${Platform.OS === "android" &&
+    `
+    elevation: 2;
+  `}
+`;
+
+export const SearchInput = styled.TextInput.attrs({
+  placeholderTextColor: "#bababa",
+  autoCapitalize: "none"
+})`
+  flex: 1;
+  height: 48px;
+  padding-horizontal: 12px;
+  color: #2d3436;
+  font-size: 16px;
+  border-right-width: 1px;
+  border-right-color: rgba(0, 0, 0, 0.1);
+`;
+
+export const SearchAddChat = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.6
+})`
+  height: 48px;
+  justify-content: center;
 `;
