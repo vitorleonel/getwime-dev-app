95fc7a4390cc14c2c2c17fc89fc2b8f4703e9f7d
Vitor Leonel <falecom@vitorleonel.com>
2020-04-01 19:47:42 -0300

    add icon

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,13 +3,34 @@ import React from "react";
 import { Container } from "./styles";
 
 import Text from "../Text";
+import Icon from "../Icon";
 
 export default function Button(props) {
+  const currentColor = !props.outlined ? "#ffffff" : "#0984E3";
+
   return (
     <Container {...props}>
-      <Text color={!props.outlined ? "#ffffff" : "#0984E3"} weight="bold">
+      {props.leftIcon && (
+        <Icon
+          name={props.leftIcon}
+          size={22}
+          color={currentColor}
+          marginRight={8}
+        />
+      )}
+
+      <Text color={currentColor} weight="bold">
         {props.text}
       </Text>
+
+      {props.rightIcon && (
+        <Icon
+          name={props.rightIcon}
+          size={22}
+          color={currentColor}
+          marginLeft={8}
+        />
+      )}
     </Container>
   );
 }
