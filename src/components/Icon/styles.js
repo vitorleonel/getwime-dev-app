a4256c2ae6fded30616ed3ab4ad2c9dbdf89afb5
Vitor Leonel <falecom@vitorleonel.com>
2020-04-01 19:48:02 -0300

    add option for add margin

--- src/components/Icon/styles.js
+++ src/components/Icon/styles.js
@@ -1,4 +1,9 @@
 import styled from "styled-components/native";
 import Icons from "@expo/vector-icons/Feather";
 
-export const Container = styled(Icons)``;
+export const Container = styled(Icons)`
+  ${props => props.marginTop && `margin-top: ${props.marginTop}px;`}
+  ${props => props.marginRight && `margin-right: ${props.marginRight}px;`}
+  ${props => props.marginBottom && `margin-bottom: ${props.marginBottom}px;`}
+  ${props => props.marginLeft && `margin-left: ${props.marginLeft}px;`}
+`;
