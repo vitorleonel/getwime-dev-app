12cde9365992a0dbcfc23db9c6d7b38362d43187
Vitor Leonel <falecom@vitorleonel.com>
2020-04-02 20:29:06 -0300

    add width property

--- src/components/Row/styles.js
+++ src/components/Row/styles.js
@@ -1,6 +1,8 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
+  ${props => props.width && `width: ${props.width}%;`}
+
   flex-direction: ${props => props.direction || "column"};
   justify-content: ${props => props.justifyContent || "flex-start"};
   align-items: ${props => props.alignItems || "stretch"};
