e084ac34fb3610594aa97d369af81ec1555d07ca
Vitor Leonel <falecom@vitorleonel.com>
2020-04-02 20:29:54 -0300

    implemen account screen

--- src/screens/Account/index.js
+++ src/screens/Account/index.js
@@ -1,7 +1,62 @@
 import React from "react";
 
-import { Container } from "./styles";
+import { Container, Image, ImageChange, Divider } from "./styles";
+
+import Row from "../../components/Row";
+import Text from "../../components/Text";
+import Icon from "../../components/Icon";
+import Field from "../../components/Field";
+import Button from "../../components/Button";
 
 export default function Account() {
-  return <Container />;
+  return (
+    <Container>
+      <Image
+        source={{
+          uri:
+            "https://ui-avatars.com/api/?name=vitorleonel&bold=true&rounded=true&size=512"
+        }}
+      >
+        <ImageChange>
+          <Icon name="camera" size={16} color="#2D3436" />
+        </ImageChange>
+      </Image>
+
+      <Row width={100} marginTop={32} paddingLeft={24} paddingRight={24}>
+        <Field label="Nome (Opcinal)" placeholder="Nome da sua conta" />
+
+        <Text color="#bababa" lineHeight={20} marginTop={16} marginBottom={16}>
+          Lembre-se sempre de manter seu anônimato, evite utilizar nome e foto
+          que te identifique, a menos que queira.
+        </Text>
+
+        <Button text="Salvar alterações" />
+      </Row>
+
+      <Row
+        width={100}
+        paddingLeft={24}
+        paddingRight={24}
+        paddingTop={32}
+        paddingBottom={32}
+      >
+        <Divider />
+      </Row>
+
+      <Row width={100} paddingLeft={24} paddingRight={24}>
+        <Text align="center" weight="bold">
+          Chave de acesso
+        </Text>
+
+        <Text color="#bababa" align="center" marginTop={8}>
+          Salve essa chave em algum lugar para que você possa entrar ou
+          recuperar sua conta.
+        </Text>
+
+        <Text color="#0984E3" align="center" marginTop={24} onPress={() => {}}>
+          ab8d36ea-4fcc-4e4a-82ce-b6c5f925637
+        </Text>
+      </Row>
+    </Container>
+  );
 }
--- src/screens/Account/styles.js
+++ src/screens/Account/styles.js
@@ -1,6 +1,47 @@
 import styled from "styled-components/native";
+import { Platform, StyleSheet } from "react-native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 export const Container = styled.View`
   flex: 1;
+  align-items: center;
   background-color: #ffffff;
+  padding-top: ${getStatusBarHeight() + 24}px;
+`;
+
+export const Image = styled.ImageBackground`
+  width: 128px;
+  height: 128px;
+  border-radius: 128px;
+  background-color: #bababa;
+
+  justify-content: flex-end;
+  align-items: flex-end;
+`;
+
+export const ImageChange = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.6
+})`
+  width: 40px;
+  height: 40px;
+  border-radius: 40px;
+  background-color: #dfe6e9;
+  justify-content: center;
+  align-items: center;
+
+  ${Platform.OS === "ios" &&
+    `
+    box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.1);
+  `}
+
+  ${Platform.OS === "android" &&
+    `
+    elevation: 3;
+  `}
+`;
+
+export const Divider = styled.View`
+  width: 100%;
+  border-bottom-width: ${StyleSheet.hairlineWidth}px;
+  border-bottom-color: #bababa;
 `;
