3c49ee0f87566ab4badf39528e9b2ec4c9edc937
Vitor Leonel <falecom@vitorleonel.com>
2020-04-09 22:22:32 -0300

    add select and define text

--- src/screens/CreateChat/index.js
+++ src/screens/CreateChat/index.js
@@ -5,7 +5,8 @@ import { Container } from "./styles";
 import Icon from "../../components/Icon";
 import Row from "../../components/Row";
 import Text from "../../components/Text";
-import Field from "../../components/Field";
+import Input from "../../components/Input";
+import Select from "../../components/Select";
 import Button from "../../components/Button";
 
 export default function CreateChat({ navigation }) {
@@ -20,26 +21,42 @@ export default function CreateChat({ navigation }) {
 
       <Row marginTop={40}>
         <Text marginBottom={24}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry.
+          Você esta a um passo de começar a construir sua própria sala de
+          bate-papo.
         </Text>
 
         <Row marginBottom={16}>
-          <Field
-            label="Nome do bate-papo"
-            placeholder="Crie um nome para seu bate-papo"
-          />
+          <Text weight="bold" marginBottom={8}>
+            Nome do bate-papo
+          </Text>
+          <Input placeholder="Crie um nome para seu bate-papo" />
         </Row>
 
         <Row marginBottom={16}>
-          <Field
-            label="Descrição"
+          <Text weight="bold" marginBottom={8}>
+            Descrição
+          </Text>
+          <Input
+            maxHeight={160}
+            multiline={true}
             placeholder="Conte um pouco do propósito desse bate-papo"
           />
         </Row>
 
         <Row>
-          <Field label="Privacidade" placeholder="Público" />
+          <Text weight="bold" marginBottom={8}>
+            Privacidade
+          </Text>
+
+          <Select
+            value="public"
+            placeholder={{}}
+            onValueChange={(value) => console.log(value)}
+            items={[
+              { label: "Público", value: "public" },
+              { label: "Não Listado", value: "unlisted" },
+            ]}
+          />
         </Row>
 
         <Button text="Criar bate-papo" marginTop={16} onPress={() => {}} />
