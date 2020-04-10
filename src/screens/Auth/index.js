c8ae65e56c436b59cf881e6149262d1d44ce6e23
Vitor Leonel <falecom@vitorleonel.com>
2020-04-09 22:21:52 -0300

    use user context to define user

--- src/screens/Auth/index.js
+++ src/screens/Auth/index.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { Container } from "./styles";
 
 import Icon from "../../components/Icon";
 import Row from "../../components/Row";
 import Text from "../../components/Text";
-import Field from "../../components/Field";
+import Input from "../../components/Input";
 import Button from "../../components/Button";
 
+import UserContext from "../../contexts/User";
+
 export default function Auth({ navigation }) {
+  const { defineUser } = useContext(UserContext);
+
   return (
     <Container>
       <Icon
@@ -20,16 +24,20 @@ export default function Auth({ navigation }) {
 
       <Row marginTop={40}>
         <Text marginBottom={24}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry.
+          Digíte sua chave de acesso abaixo para entrar na sua conta.
         </Text>
 
-        <Field
-          label="Chave de acesso"
-          placeholder="Digite sua chave de acesso"
-        />
+        <Row marginBottom={16}>
+          <Text weight="bold" marginBottom={8}>
+            Chave de acesso
+          </Text>
+          <Input placeholder="Digite sua chave de acesso" />
+        </Row>
 
-        <Button text="Entrar" marginTop={16} onPress={() => {}} />
+        <Button
+          text="Entrar"
+          onPress={() => defineUser({ name: "Vitor Leonel" })}
+        />
       </Row>
     </Container>
   );
