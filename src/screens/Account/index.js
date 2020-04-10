d0d167ac412d77458d7162840fd529112a86fdb3
Vitor Leonel <falecom@vitorleonel.com>
2020-04-09 22:21:37 -0300

    function to copy key for clipboard

--- src/screens/Account/index.js
+++ src/screens/Account/index.js
@@ -1,20 +1,30 @@
 import React from "react";
+import { Clipboard, Alert } from "react-native";
 
 import { Container, Image, ImageChange, Divider } from "./styles";
 
 import Row from "../../components/Row";
 import Text from "../../components/Text";
 import Icon from "../../components/Icon";
-import Field from "../../components/Field";
+import Input from "../../components/Input";
 import Button from "../../components/Button";
 
 export default function Account() {
+  function copyKeyAccess() {
+    Clipboard.setString("ab8d36ea-4fcc-4e4a-82ce-b6c5f925637");
+
+    Alert.alert(
+      "Chave de acesso copiada!",
+      "Salve essa chave em algum lugar para que você possa entrar ou recuperar sua conta."
+    );
+  }
+
   return (
     <Container>
       <Image
         source={{
           uri:
-            "https://ui-avatars.com/api/?name=vitorleonel&bold=true&rounded=true&size=512"
+            "https://ui-avatars.com/api/?name=vitorleonel&bold=true&rounded=true&size=512",
         }}
       >
         <ImageChange>
@@ -23,7 +33,12 @@ export default function Account() {
       </Image>
 
       <Row width={100} marginTop={32} paddingLeft={24} paddingRight={24}>
-        <Field label="Nome (Opcinal)" placeholder="Nome da sua conta" />
+        <Row>
+          <Text weight="bold" marginBottom={8}>
+            Nome
+          </Text>
+          <Input placeholder="Nome da sua conta" value="as812as1" />
+        </Row>
 
         <Text color="#bababa" lineHeight={20} marginTop={16} marginBottom={16}>
           Lembre-se sempre de manter seu anônimato, evite utilizar nome e foto
@@ -53,7 +68,12 @@ export default function Account() {
           recuperar sua conta.
         </Text>
 
-        <Text color="#0984E3" align="center" marginTop={24} onPress={() => {}}>
+        <Text
+          color="#0984E3"
+          align="center"
+          marginTop={24}
+          onPress={() => copyKeyAccess("ab8d36ea-4fcc-4e4a-82ce-b6c5f925637")}
+        >
           ab8d36ea-4fcc-4e4a-82ce-b6c5f925637
         </Text>
       </Row>
